a544ab7f46bff79b330d2f1a08012ff9ceca6a66
Ishmael <hello@ishmaelavila.com>
2022-05-08 10:04:48 -0500

    test: add test for reading line

--- internal/textreader/textreader_test.go
+++ internal/textreader/textreader_test.go
@@ -1,8 +1,11 @@
 package textreader_test
 
 import (
+	"bufio"
 	"errors"
 	"ishmaelavila/amextocsv/internal/textreader"
+	"os"
+	"strings"
 	"testing"
 )
 
@@ -19,5 +22,38 @@ func TestTextReader(t *testing.T) {
 		if !errors.Is(err, textreader.ErrEmptyFilePath) {
 			t.Fatalf("wrong error, got: %s want: %s", err, textreader.ErrEmptyFilePath)
 		}
+	})
+
+	t.Run("reads a line", func(t *testing.T) {
+		reader := textreader.TextReader{}
+
+		lineExpected := "May 6"
+
+		linesToScan := strings.NewReader(`May 6
+		Pending
+		
+		CHICK-FIL-A
+		
+		$27.48`)
+
+		scanner := bufio.NewScanner(linesToScan)
+
+		reader.Scanner = *scanner
+		reader.OpenedFile = os.File{}
+
+		text, err := reader.ReadLine()
+
+		if err != nil {
+			t.Fatalf("unexpected error reading line %s", err)
+		}
+
+		if text == nil {
+			t.Fatalf("error, read text was nil")
+		}
+
+		if *text != lineExpected {
+			t.Fatalf("error unexpected string, got %s, want %s", *text, lineExpected)
+		}
+
 	})
 }
