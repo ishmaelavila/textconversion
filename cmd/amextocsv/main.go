854128cb3108169076c8f5d18f8a4616f24bb1b5
Ishmael <hello@ishmaelavila.com>
2022-05-12 21:02:35 -0500

    feature: add flag to reverse line order of output by default

--- cmd/amextocsv/main.go
+++ cmd/amextocsv/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	amexsitetocsv "ishmaelavila/amextocsv/internal/amexsite-to-csv"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -13,14 +15,16 @@ func main() {
 	argsExpected := 2
 
 	if len(args) > argsExpected {
-		log.Printf("Invalid number of arguments, expected %d, got %d, ignoring extra arguments", argsExpected, len(args))
+		log.Printf("invalid number of arguments, expected %d, got %d, ignoring extra arguments", argsExpected-1, len(args))
 	}
 
 	if len(args) < argsExpected {
-		log.Fatalf("Invalid number of arguments, please provide a path")
+		log.Fatalf("invalid number of arguments. Usage amextocsv")
 	}
 
 	path := args[1]
+
+	reverseOutput := flag.Bool("r", true, "reverse output of CSV")
 
 	converter, err := amexsitetocsv.New(path)
 
@@ -29,6 +33,10 @@ func main() {
 	}
 
 	csvString := converter.ConvertToCsv()
+
+	if *reverseOutput {
+		csvString = reverse(csvString)
+	}
 
 	fmt.Println(csvString)
 
@@ -46,4 +54,18 @@ func main() {
 
 	os.Exit(0)
 
+}
+
+func reverse(s string) string {
+	lines := strings.Split(s, "\n")
+
+	reverseOrderSlice := []string{}
+
+	for i := range lines {
+		line := strings.TrimSpace(lines[len(lines)-1-i])
+		reverseOrderSlice = append(reverseOrderSlice, line+"\n")
+	}
+
+	reversedString := strings.Join(reverseOrderSlice, "")
+	return reversedString
 }
